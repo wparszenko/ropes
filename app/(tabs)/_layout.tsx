a22cf4d8c5092751e34920b26a9e296dee48d7b3
wparszenko <gu.rodfam.5@gmail.com>
2025-06-17 14:46:10 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Play, Trophy, Settings } from 'lucide-react-native';
+import { Chrome as Home, Play, Trophy, Settings } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (

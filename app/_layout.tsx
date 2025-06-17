fed70a8ee28ba21558beeaca1efe44e388dc2681
wparszenko <gu.rodfam.5@gmail.com>
2025-06-17 16:52:46 +0200

    Fix Expo Web Development Server

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="light" />
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable web support
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
 module.exports = withNativeWind(config, { input: './global.css' });
